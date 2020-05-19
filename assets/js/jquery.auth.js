b9eb4890fb80169cc23919e5f4e122aee77acb32
Ta Long Khanh <longkhanh02092000@gmail.com>
2020-05-19 20:16:30 +0700

    Feature login-register + jquery

---
+++ assets/js/jquery.auth.js
@@ -0,0 +1,46 @@
+$(function () {
+    // login
+    $('.login-click').click(function (e) { 
+        e.preventDefault();
+        $('.form__login').addClass('hienra');       
+    });
+
+    $('.btn--back').click(function (e) { 
+        e.preventDefault();
+        $('.form__login').removeClass('hienra');
+        
+    });
+    $('.form__login-overlay').click(function (e) { 
+        e.preventDefault();
+        $('.form__login').removeClass('hienra');
+    });
+
+    $('.btn-register').click(function (e) { 
+        e.preventDefault();
+        $('.form__login').removeClass('hienra');
+        $('.form__register').addClass('hienra');
+
+    });
+
+    // register
+    $('.register-click').click(function (e) { 
+        e.preventDefault();
+        $('.form__register').addClass('hienra');       
+    });
+
+    $('.btn--back').click(function (e) { 
+        e.preventDefault();
+        $('.form__register').removeClass('hienra');
+    });
+    $('.form__register-overlay').click(function (e) { 
+        e.preventDefault();
+        $('.form__register').removeClass('hienra');
+    });
+
+    $('.btn-login').click(function (e) { 
+        e.preventDefault();
+        $('.form__register').removeClass('hienra');
+        $('.form__login').addClass('hienra');
+    });
+
+});
